836d96b11d4f2f855c6c1e51760f8cfdb01ac380
bzjerome <60231542+bzjerome@users.noreply.github.com>
2020-01-23 09:06:04 -0800

    Lab 0

---
+++ Lab0.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 23 08:58:09 2020
+
+@author: jero7605
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.signal as sig
+import control as con
+import pandas as pd
+
+############################################
+#                                          #
+# Brady Jerome                             #
+# ECE351-52                                #
+# Lab 0                                    #
+# January 23, 2019                         #
+#                                          #
+############################################
+
+
+# Question 1
+#   Last summer I mostly just worked and spent time with my family and friends.
+#   Next summer, I'm taking a trip to Hawaii in June.
+
+
+# Question 2
+#   From this lab, I want to learn how to properly code in python.
+#   I've heard that it's very useful to have an understanding of python for
+#   interviews and the workplace in general. So it would be very useful to
+#   gain a good understanding of python
